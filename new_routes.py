b4521e1aec02ee9fa20ef57da3e48df4ccd25db2
Gabriele Girelli <gigi.ga90@gmail.com>
2019-02-04 13:55:01 +0100

    Enabled CORS

--- new_routes.py
+++ new_routes.py
@@ -8,6 +8,22 @@ pdApp.vd['breadcrumbs'] = True
 pdApp.vd['SHOW_COOKIE_CONSENT_BANNER'] = True
 pdApp.vd['menu_template'] = "design_menu.tpl"
 
+class EnableCors(object):
+	'''https://stackoverflow.com/a/17262900/1593536'''
+    name = 'enable_cors'
+    api = 2
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+            if request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+        return _enable_cors
+pdApp.install(EnableCors())
+
 @root.route("/browse")
 @root.route("/browse/")
 @bot.view("browse")
